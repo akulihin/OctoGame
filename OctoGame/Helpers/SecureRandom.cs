ca7b3ca4b6b59835f1a3994fbac044cc8e3007d9
Artem Kulihin <duraek@gmail.com>
2019-01-08 00:55:57 -0500

    removed static class

--- OctoGame/Helpers/SecureRandom.cs
+++ OctoGame/Helpers/SecureRandom.cs
@@ -41,44 +41,4 @@ namespace OctoGame.Helpers
             return buffer;
         }
     }
-
-    public class SecureRandomStatic
-    {
-        private static readonly RNGCryptoServiceProvider Generator = new RNGCryptoServiceProvider();
-
-        public static int Random(int min, int max)
-        {
-
-            var randomNumber = new byte[8];
-            Generator.GetBytes(randomNumber);
-            var asc2ConvertBytes = Convert.ToDouble(randomNumber[0]);
-
-            var multy = Math.Max(0, asc2ConvertBytes / 255d);
-            var range = max - min + 1;
-            var randomInRange = Math.Floor(multy * range);
-
-            if (randomInRange > max)
-                randomInRange = max;
-
-            return (int) (min + randomInRange);
-        }
-
-        public static int Random(int max)
-        {
-            var randomNumber = new byte[1];
-
-            Generator.GetBytes(randomNumber);
-
-            var asc2ConvertBytes = Convert.ToDouble(randomNumber[0]);
-
-            var multy = Math.Max(0, asc2ConvertBytes / 255d);
-            var range = max - 0 + 1;
-            var randomInRange = Math.Floor(multy * range);
-
-            var returnValue = (int) (0 + randomInRange);
-            if (returnValue >= max)
-                returnValue = max;
-            return returnValue;
-        }
-    }
 }
